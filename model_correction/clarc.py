57400868f72f1e876fcb1bafb677d6843b94337c
Michal Wilinski <m.wilinski11@gmail.com>
2024-10-29 15:53:48 +0100

    cleanup

--- model_correction/clarc.py
+++ model_correction/clarc.py
@@ -1,5 +1,3 @@
-# pclarc.py
-
 import torch
 from torch import nn
 
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debias",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchvision",
+        "numpy",
+        "matplotlib",
+        "Pillow",
+        "tqdm",
+        "scipy",
+        "scikit-image",
+        "scikit-learn",
+        "pandas",
+        "opencv-python",
+        "wandb",
+        "albumentations",
+        "pytorch-lightning",
+        "zennit-crp",
+        "pprint",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',  # Specify the Python version compatibility
+)
