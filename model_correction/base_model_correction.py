f13fb5e4b2be51263f3d0288e21f0bedf3ae6f61
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-11-24 23:06:57 +0100

    Update wrapper

--- model_correction/base_model_correction.py
+++ model_correction/base_model_correction.py
@@ -3,22 +3,7 @@ import torch
 from torch import nn
 
 from ..cavs import extract_activations, compute_mass_mean_probe, compute_cav
-
-
-# Wrapper for requiring activations and CAVs to be computed before applying model correction
-def require_activations_and_cav(func):
-    def wrapped(self, cav_layer: str, *args, **kwargs):
-        if not hasattr(self, "activations"):
-            raise ValueError(
-                "Activations must be computed before applying model correction"
-            )
-
-        if not hasattr(self, "cav"):
-            raise ValueError("CAVs must be computed before applying model correction")
-
-        return func(self, cav_layer, *args, **kwargs)
-
-    return wrapped
+from .helpers import require_activations_and_cav
 
 
 class ModelCorrectionMethod(ABC):
