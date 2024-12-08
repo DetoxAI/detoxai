ef4b22f2b035c2c6d7c6f9dec7262da0223369b7
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-12-08 22:39:29 +0100

    Fix import

--- model_correction/__init__.py
+++ model_correction/__init__.py
@@ -2,6 +2,6 @@ from .hooks import add_clarc_hook, add_mass_mean_probe_hook  # noqa
 from .clarc import CLARC  # noqa
 from .aclarc import ACLARC  # noqa
 from .pclarc import PCLARC  # noqa
-from .rrclarc import RRCLARC  # noqa
+from .rrclarc import RRCLARC, RRLossType, RRMaskingPattern  # noqa
 from .model_correction import ModelCorrectionMethod  # noqa
 from .leace import LEACE  # noqa
