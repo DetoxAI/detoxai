1ef943cff837ca055f02b454e2d14a45805bcff3
Ignacy Stępka <ignacy.stepka@gmail.com>
2025-01-23 22:14:57 +0100

    Add square pad to fixed dims

--- src/detoxai/utils/transformations.py
+++ src/detoxai/utils/transformations.py
@@ -5,9 +5,15 @@ import torchvision.transforms.functional as F
 
 
 class SquarePad:
+    def __init__(self, resize_to: int | None = None):
+        self.resize_to = resize_to
+
     def __call__(self, image):
         w, h = image.size
-        max_wh = np.max([w, h])
+        if self.resize_to:
+            max_wh = self.resize_to
+        else:
+            max_wh = np.max([w, h])
         hp = int((max_wh - w) / 2)
         vp = int((max_wh - h) / 2)
         padding = (hp, vp, hp, vp)
